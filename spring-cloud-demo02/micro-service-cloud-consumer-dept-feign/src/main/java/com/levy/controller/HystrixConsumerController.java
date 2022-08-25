f5b69338def9505703c8b3f8e2dd12835e5e08d4
liwei515 <liwei515@ke.com>
2022-08-25 20:15:21 +0800

    spring cloud hystrix 全局降级

--- spring-cloud-demo02/micro-service-cloud-consumer-dept-feign/src/main/java/com/levy/controller/HystrixConsumerController.java
+++ spring-cloud-demo02/micro-service-cloud-consumer-dept-feign/src/main/java/com/levy/controller/HystrixConsumerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.levy.service.DeptHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,24 +14,38 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 @RestController
+@DefaultProperties(defaultFallback = "deptGlobalFallbackMethod")
 public class HystrixConsumerController {
+
   @Resource
   private DeptHystrixService deptHystrixService;
+
   @RequestMapping(value = "/consumer/dept/hystrix/ok/{id}")
   public String deptInfoOk(@PathVariable("id") Integer id) {
     return deptHystrixService.deptInfoOk(id);
   }
-  //在客户端进行降级
+
+  /**
+   * 在客户端进行降级
+   */
   @RequestMapping(value = "/consumer/dept/hystrix/timeout/{id}")
-  @HystrixCommand(fallbackMethod = "deptTimeoutHandler") // 为该请求指定专属的回退方法
+//  @HystrixCommand(fallbackMethod = "deptTimeoutHandler") // 为该请求指定专属的回退方法
+  @HystrixCommand
   public String deptInfoTimeout(@PathVariable("id") Integer id) {
     String s = deptHystrixService.deptInfoTimeout(id);
     log.info(s);
     return s;
   }
-  // deptInfo_Timeout方法的 专用 fallback 方法
+
+  /**
+   * deptInfoTimeout方法的 专用 fallback 方法
+   */
   public String deptTimeoutHandler(@PathVariable("id") Integer id) {
     log.info("deptInfoTimeout 出错，服务已被降级！");
     return "服务端系统繁忙，请稍后再试！（客户端 deptInfoTimeout 专属的回退方法触发）";
+  }
+
+  public String deptGlobalFallbackMethod(){
+    return "运行出错或服务端系统繁忙，请稍后再试！（客户端全局回退方法触发）";
   }
 }
