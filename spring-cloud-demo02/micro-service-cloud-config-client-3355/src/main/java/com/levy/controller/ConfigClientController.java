96010f44e95569f09306f79091e69cecb8b937e9
liwei515 <liwei515@ke.com>
2022-08-26 14:22:18 +0800

    spring cloud config 手动刷新配置

--- spring-cloud-demo02/micro-service-cloud-config-client-3355/src/main/java/com/levy/controller/ConfigClientController.java
+++ spring-cloud-demo02/micro-service-cloud-config-client-3355/src/main/java/com/levy/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.levy.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author liwei515
  */
+
+@RefreshScope  // 为了让动态（手动）的获取最新的git 配置，在添加 actuator 监控加载 RefreshScope，
 @RestController
 public class ConfigClientController {
 
