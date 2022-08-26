935b1581f0c8c9e96a4cbddf82e2489b67b723f1
liwei515 <liwei515@ke.com>
2022-08-26 12:33:56 +0800

    spring cloud config 搭建config客户端（不重启Config客户端就无法获取最新配置）

---
+++ spring-cloud-demo02/micro-service-cloud-config-client-3355/src/main/java/com/levy/controller/ConfigClientController.java
@@ -0,0 +1,28 @@
+package com.levy.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 读取配置中心指定配置文件的内容，并展示到页面
+ *
+ * @author liwei515
+ */
+@RestController
+public class ConfigClientController {
+
+  @Value("${server.port}")
+  private String serverPort;
+
+  @Value("${config.info}")
+  private String configInfo;
+
+  @Value("${config.version}")
+  private String configVersion;
+
+  @GetMapping(value = "/getConfig")
+  public String getConfig() {
+    return "info：" + configInfo + "<br/>version：" + configVersion + "<br/>port：" + serverPort;
+  }
+}
